4b23f4e2bde0321cae3aa4685064f5d6c4ed4824
Hector Polanco <hdp2112@columbia.edu>
2019-10-16 04:43:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -129,6 +129,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
-  return programmer_hash
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 end
